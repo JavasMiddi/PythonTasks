b3c04cd326db35795d3d6965d195bde954cab345
JavasMiddi <68111694+JavasMiddi@users.noreply.github.com>
2020-07-17 15:19:14 +0100

    Add files via upload

---
+++ GradeCals.py
@@ -0,0 +1,7 @@
+mark = int(input("Please input a mark: "))
+if mark > 85:
+    print("Distinction!")
+elif 65 < mark < 85:
+    print("Pass!")
+else:
+    print("Fail!")
---
+++ Planets.py
@@ -0,0 +1,5 @@
+planets = ['Mars', 'Jupiter', 'Uranus', 'Earth', 'Saturn', 'Pluto', 'Mercury', 'Venus']
+for i in planets:
+    if i == 'Pluto':
+        continue
+    print(i)
